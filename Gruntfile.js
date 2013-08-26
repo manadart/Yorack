78941ad4a6bcfd184af2bd610d1954b53d1d4ab6
SpiritMachine <jphillips.pro@gmail.com>
2013-08-26 11:24:58 +0200

    Updated with Gruntfile generated with 'yo webapp' including Bootstrap for SASS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2013-08-23 using generator-webapp 0.3.0
+// Generated on 2013-08-26 using generator-webapp 0.3.0
 'use strict';
 var LIVERELOAD_PORT = 35729;
 var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
